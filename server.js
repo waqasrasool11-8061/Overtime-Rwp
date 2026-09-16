933b08ec6a0444641ea1c7db3c42570b814274f4
WAQAS RASOOL <waqasrasool11@gmail.com>
2026-09-17 02:16:19 +0500

    Increase express body parser payload limit to 150mb to allow large Excel imports

--- server.js
+++ server.js
@@ -541,7 +541,8 @@ function validateSearchRecord(record, employeeNameSet) {
   return "";
 }
 
-app.use(express.json({ limit: "2mb" }));
+app.use(express.json({ limit: "150mb" }));
+app.use(express.urlencoded({ limit: "150mb", extended: true }));
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   const isLocal = origin && (
